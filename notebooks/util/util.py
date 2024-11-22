d27d359477768643f725a467e24653cc51130276
Marc Thieme <marcthieme@proton.me>
2024-11-22 22:38:00 +0100

    Copy 2 functions from lecture

--- notebooks/util/util.py
+++ notebooks/util/util.py
@@ -1,1 +1,78 @@
+import pandas as pd
+from matplotlib import pyplot as plt
 
+# Configuration
+anomaly_color = 'sandybrown'
+prediction_color = 'yellowgreen'
+training_color = 'yellowgreen'
+validation_color = 'gold'
+test_color = 'coral'
+figsize = (9, 3)
+
+
+def plot_autocorrelation(data, max_lag=100, figsize=figsize):
+    # Open a new figure
+    plt.close('all')
+    plt.figure(figsize=figsize)
+    # Autocorrelation plot
+    pd.plotting.autocorrelation_plot(data['value'])
+    # Customized x limits
+    plt.xlim(0, max_lag)
+    # Rotated x ticks
+    plt.xticks(rotation=45)
+    plt.grid(':')
+    plt.tight_layout()
+
+
+def plot_series(data, labels=None,
+                windows=None,
+                predictions=None,
+                highlights=None,
+                val_start=None,
+                test_start=None,
+                threshold=None,
+                figsize=figsize,
+                xlabel=None,
+                ylabel=None):
+    # Open a new figure
+    plt.close('all')
+    plt.figure(figsize=figsize)
+    # Plot data
+    plt.plot(data.index, data.values, zorder=0)
+    # Rotated x ticks
+    plt.xticks(rotation=45)
+    # Plot labels
+    if labels is not None:
+        plt.scatter(labels.values, data.loc[labels],
+                    color=anomaly_color, zorder=2)
+    # Plot windows
+    if windows is not None:
+        for _, wdw in windows.iterrows():
+            plt.axvspan(wdw['begin'], wdw['end'],
+                        color=anomaly_color, alpha=0.3, zorder=1)
+
+    # Plot training data
+    if val_start is not None:
+        plt.axvspan(data.index[0], val_start,
+                    color=training_color, alpha=0.1, zorder=-1)
+    if val_start is None and test_start is not None:
+        plt.axvspan(data.index[0], test_start,
+                    color=training_color, alpha=0.1, zorder=-1)
+    if val_start is not None:
+        plt.axvspan(val_start, test_start,
+                    color=validation_color, alpha=0.1, zorder=-1)
+    if test_start is not None:
+        plt.axvspan(test_start, data.index[-1],
+                    color=test_color, alpha=0.3, zorder=0)
+    # Predictions
+    if predictions is not None:
+        plt.scatter(predictions.values, data.loc[predictions],
+                    color=prediction_color, alpha=.4, zorder=3)
+    # Plot threshold
+    if threshold is not None:
+        plt.plot([data.index[0], data.index[-1]], [threshold,
+                 threshold], linestyle=':', color='tab:red')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.grid(':')
+    plt.tight_layout()
